4e35497ab76eec148d93a87967b6c20712f368ea
Sabashan Ragavan <sabes.ragavan@gmail.com>
2013-05-25 16:26:44 -0400

    Updating Chapter 2

--- Chapter 2/Exercise2-5.c
+++ Chapter 2/Exercise2-5.c
@@ -1,6 +1,6 @@
-//Write a function any(s1,s2) which returns the first location
-//in the string s1 where any character from the string s2 occurs 
-//or -1 if s1 contains no character from s2. 
+/* Write a function any(s1,s2) which returns the first location
+in the string s1 where any character from the string s2 occurs 
+or -1 if s1 contains no character from s2. */
 
 #include <stdio.h> 
 
--- Chapter 2/Exercise2-9.c
+++ Chapter 2/Exercise2-9.c
@@ -1,5 +1,5 @@
-//In a two's complement number system, x &= (x-1) deletes the rightmost 1-bit
-//in x. Explain why. Use this observation to write a faster version of bitcount
+/* In a two's complement number system, x &= (x-1) deletes the rightmost 1-bit
+in x. Explain why. Use this observation to write a faster version of bitcount */
 
 #include <stdio.h> 
 
